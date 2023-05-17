16a07abe4bcf900d77cbf624a88fd927968b8653
euerickraphael <ersa2@aluno.ifal.edu.br>
2023-05-17 17:26:35 -0300

    1.0.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
+import React, { useState } from 'react'
+
+
+
+
 
 function App() {
+
+  const [ dado, setDado ] = useState([""]);
+
+
+  function receberDados () {
+    axios.get("http://localhost:80/Receber")
+    .then(response => {
+      setDado(response.data)
+    })
+  } 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{dado}</h3>
+      <button onClick={() => receberDados()}>Receber Dados</button>
     </div>
   );
 }
